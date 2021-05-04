b39940ed45625d5d2005d62c3cd52ab0b26728ba
Andrew Zaloga <63148692+fuckingsore@users.noreply.github.com>
2021-05-04 10:22:30 +0300

    Delete working with files directory

--- working with files/module fs/readSync.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-fs.mkdirSync('./SecondDirSync',0o777);
-fs.writeFileSync('./SecondDirSync/messageFile.txt', 'Hello World & Node');
-var data = fs.readFileSync('./SecondDirSync/messageFile.txt','UTF-8');
-console.log('File created with contents:');
-console.log(data);
